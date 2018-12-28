12205d238c517cf2f3e6dcfa962fa92c14d067f8
Andrei-Marius MUNTEA <amuntea@bitdefender.com>
2018-12-28 22:22:54 +0200

    Beautified the code

--- DataStructuresTests/SynchronizedQueueTests.cpp
+++ DataStructuresTests/SynchronizedQueueTests.cpp
@@ -6,9 +6,8 @@
 #include "..\DataStructuresLibrary\SynchronizedQueue.c"
 
 #include <Windows.h>
+#include <vector>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-#define QUEUE_ELMENT_TEST_TAG   'TEQ#'  //#QET
 
 namespace DataStructuresTests
 {
@@ -19,18 +18,20 @@ namespace DataStructuresTests
 
     typedef struct _SYNCHRONIZED_QUEUE_THREAD_CONTEXT
     {
-        PSYNCHRONIZED_QUEUE Queue;
-        volatile long long* Elements;
-        unsigned int        NoElements;
+        PSYNCHRONIZED_QUEUE     Queue;
+        std::vector<long long>* Elements;
+        size_t                  NoElements;
     }SYNCHRONIZED_QUEUE_THREAD_CONTEXT, *PSYNCHRONIZED_QUEUE_THREAD_CONTEXT;
 
 
     DWORD WINAPI SynchronizedQueueTestThreadPushRoutine(PVOID Context)
     {
-        PSYNCHRONIZED_QUEUE_THREAD_CONTEXT context = (PSYNCHRONIZED_QUEUE_THREAD_CONTEXT)(Context);
-        for (unsigned int i = 0; i < context->NoElements; ++i)
+        auto context = (PSYNCHRONIZED_QUEUE_THREAD_CONTEXT)(Context);
+        auto elements = context->Elements;
+
+        for(size_t i = 0; i < context->NoElements; ++i)
         {
-            DS_STATUS status = SynchronizedQueuePush(context->Queue, (PVOID)(&context->Elements[i]));
+            DS_STATUS status = SynchronizedQueuePush(context->Queue, (PVOID)(&elements->at(i)));
             Assert::IsTrue(DS_STATUS_SUCCESS(status));
         }
 
@@ -39,18 +40,17 @@ namespace DataStructuresTests
 
     DWORD WINAPI SynchronizedQueueTestThreadPopRoutine(PVOID Context)
     {
-        PSYNCHRONIZED_QUEUE_THREAD_CONTEXT context = (PSYNCHRONIZED_QUEUE_THREAD_CONTEXT)(Context);
-        
-        for (unsigned int i = 0; i < context->NoElements; ++i)
+        auto context = (PSYNCHRONIZED_QUEUE_THREAD_CONTEXT)(Context);
+        auto elements = context->Elements;
+        long long* element = nullptr;
+
+        for (size_t i = 0; i < context->NoElements; ++i)
         {
-            int* element = NULL;
-            
-            DS_STATUS status = SynchronizedQueuePop(context->Queue, (PVOID*)&element);
+            auto status = SynchronizedQueuePop(context->Queue, (PVOID*)&element);
             Assert::IsTrue(DS_STATUS_SUCCESS(status));
 
-            InterlockedIncrement64(&context->Elements[*element]);
+            InterlockedIncrement64(&elements->at(*element));
         }
-
         return 0;
     }
 
@@ -59,167 +59,108 @@ namespace DataStructuresTests
     public:
         TEST_METHOD(QueuePushPop500From8ThreadsTest)
         {
-            CrtCheckMemory __leaks;
-
-            SYNCHRONIZED_QUEUE queue = { 0 };
-            SynchronizedQueueInitialize(&queue, &SynchronizedQueueFreeRoutine);
-            
-            const unsigned int noThreads = 8;
-            const unsigned int noElements = 500;
-            const unsigned int modulo = 10;
-
-            volatile long long Elements[noThreads * noElements] = { 0 };
-            volatile long long Check[modulo] = { 0 };
-
-            for (unsigned int i = 0; i < noThreads * noElements; ++i) Elements[i] = i % modulo;
-
-            PushNFromXThreads(&queue, noElements, noThreads, Elements);
-            PopNFromXThreads(&queue, noElements, noThreads, Check);
-            CheckResult((noElements* noThreads) / modulo, Check, modulo);
-
-            SynchronizedQueueDestroy(&queue);
+            TestPushPop(8, 500, 10);
         }
 
         TEST_METHOD(QueuePushPop500From2ThreadsTest)
         {
-            CrtCheckMemory __leaks;
-
-            SYNCHRONIZED_QUEUE queue = { 0 };
-            SynchronizedQueueInitialize(&queue, &SynchronizedQueueFreeRoutine);
-
-            const unsigned int noThreads = 2;
-            const unsigned int noElements = 500;
-            const unsigned int modulo = 10;
-
-            volatile long long Elements[noThreads * noElements] = { 0 };
-            volatile long long Check[modulo] = { 0 };
-
-            for (unsigned int i = 0; i < noThreads * noElements; ++i) Elements[i] = i % modulo;
-
-            PushNFromXThreads(&queue, noElements, noThreads, Elements);
-            PopNFromXThreads(&queue, noElements, noThreads, Check);
-            CheckResult((noElements* noThreads) / modulo, Check, modulo);
-
-            SynchronizedQueueDestroy(&queue);
+            TestPushPop(2, 500, 10);
         }
 
         TEST_METHOD(QueuePushPop6000From16ThreadsTest)
         {
-            CrtCheckMemory __leaks;
-
-            SYNCHRONIZED_QUEUE queue = { 0 };
-            SynchronizedQueueInitialize(&queue, &SynchronizedQueueFreeRoutine);
-
-            const unsigned int noThreads = 16;
-            const unsigned int noElements = 6000;
-            const unsigned int modulo = 10;
-
-            volatile long long Elements[noThreads * noElements] = { 0 };
-            volatile long long Check[modulo] = { 0 };
-
-            for (unsigned int i = 0; i < noThreads * noElements; ++i) Elements[i] = i % modulo;
-
-            PushNFromXThreads(&queue, noElements, noThreads, Elements);
-            PopNFromXThreads(&queue, noElements, noThreads, Check);
-            CheckResult((noElements* noThreads) / modulo, Check, modulo);
-
-            SynchronizedQueueDestroy(&queue);
+            TestPushPop(16, 6000, 10);
         }
 
         TEST_METHOD(QueuePushPop1000000From32ThreadsTest)
         {
+           TestPushPop(32, 1000000, 10);
+        }
+
+        void 
+        PushNFromXThreads(
+            _Inout_ PSYNCHRONIZED_QUEUE Queue,
+            _In_    unsigned int NoThreads,
+            _In_    unsigned int NoElements,
+            _Inout_ std::vector<long long>* Elements
+        )
+        {
+            PushPopNFromXThreads(Queue, NoThreads, NoElements, Elements, SynchronizedQueueTestThreadPushRoutine);
+        }
+
+        void 
+        PopNFromXThreads(
+            _Inout_ PSYNCHRONIZED_QUEUE Queue,
+            _In_    unsigned int NoThreads,
+            _In_    unsigned int NoElements,
+            _Inout_ std::vector<long long>* Elements
+        )
+        {
+            PushPopNFromXThreads(Queue, NoThreads, NoElements, Elements, SynchronizedQueueTestThreadPopRoutine);
+        }
+
+        void 
+        CheckResult(
+            _In_  long long ExpectedCount, 
+            _In_  const std::vector<long long>& Elements
+        )
+        {
+            for (auto& element : Elements)
+            {
+                Assert::IsTrue(element == ExpectedCount);
+            }
+        }
+
+        void 
+        TestPushPop(
+            _In_ unsigned int NoThreads, 
+            _In_ unsigned int NoElements, 
+            _In_ unsigned int Modulo
+        )
+        {
             CrtCheckMemory __leaks;
+
+            std::vector<long long> elements(NoThreads * NoElements, 0);
+            std::vector<long long> check(Modulo, 0);
 
             SYNCHRONIZED_QUEUE queue = { 0 };
             SynchronizedQueueInitialize(&queue, &SynchronizedQueueFreeRoutine);
 
-            const unsigned int noThreads = 32;
-            const unsigned int noElements = 1000000;
-            const unsigned int modulo = 10;
+            for (unsigned int i = 0; i < NoThreads * NoElements; ++i) elements[i] = i % Modulo;
 
-            volatile long long* Elements = (volatile long long*)DS_Alloc(noThreads * noElements * sizeof(volatile long long), QUEUE_ELMENT_TEST_TAG);
-            volatile long long Check[modulo] = { 0 };
-
-            Assert::IsTrue(Elements != NULL);
-            for (unsigned int i = 0; i < noThreads * noElements; ++i) Elements[i] = i % modulo;
-
-            PushNFromXThreads(&queue, noElements, noThreads, Elements);
-            PopNFromXThreads(&queue, noElements, noThreads, Check);
-            CheckResult((noElements* noThreads) / modulo, Check, modulo);
+            PushNFromXThreads(&queue, NoThreads, NoElements, &elements);
+            PopNFromXThreads(&queue, NoThreads, NoElements, &check);
+            CheckResult((NoElements * NoThreads) / Modulo, check);
 
             SynchronizedQueueDestroy(&queue);
-            DS_Free((void*)Elements, QUEUE_ELMENT_TEST_TAG);
         }
 
-        void PushNFromXThreads(PSYNCHRONIZED_QUEUE Queue, unsigned int N, unsigned int X, volatile long long* Elements)
+        void PushPopNFromXThreads(
+            _Inout_ PSYNCHRONIZED_QUEUE Queue,
+            _In_    unsigned int NoThreads,
+            _In_    unsigned int NoElements,
+            _Inout_ std::vector<long long>* Elements, 
+            _In_    LPTHREAD_START_ROUTINE Routine
+        )
         {
-            PushPopNFromXThreads(Queue, N, X, Elements, SynchronizedQueueTestThreadPushRoutine);
-        }
+            std::vector<SYNCHRONIZED_QUEUE_THREAD_CONTEXT> contexts(NoThreads);
+            std::vector<HANDLE> threads(NoThreads, nullptr);
 
-        void PopNFromXThreads(PSYNCHRONIZED_QUEUE Queue, unsigned int N, unsigned int X, volatile long long* Elements)
-        {
-            PushPopNFromXThreads(Queue, N, X, Elements, SynchronizedQueueTestThreadPopRoutine);
-        }
-
-        void CheckResult(int ExpectedCount, volatile long long* Elements, unsigned int ElementsCount)
-        {
-            for (unsigned int i = 0; i < ElementsCount; ++i)
-            {
-                Assert::IsTrue(Elements[i] == ExpectedCount);
-            }
-        }
-
-        void PushPopNFromXThreads(PSYNCHRONIZED_QUEUE Queue, unsigned int N, unsigned int X, volatile long long* Elements, LPTHREAD_START_ROUTINE Routine)
-        {
-            PSYNCHRONIZED_QUEUE_THREAD_CONTEXT contexts = NULL;
-            HANDLE* threads = NULL;
-            bool shouldFail = true;
-
-            contexts = (PSYNCHRONIZED_QUEUE_THREAD_CONTEXT)DS_Alloc(X * sizeof(_SYNCHRONIZED_QUEUE_THREAD_CONTEXT), QUEUE_ELMENT_TEST_TAG);
-            if (NULL == contexts)
-            {
-                goto CleanUp;
-            }
-
-            threads = (HANDLE*)DS_Alloc(X * sizeof(HANDLE), QUEUE_ELMENT_TEST_TAG);
-            if (NULL == threads)
-            {
-                goto CleanUp;
-            }
-            Ds_ZeroMemory(threads, X * sizeof(HANDLE));
-
-            for (unsigned int i = 0; i < X; ++i)
+            for (unsigned int i = 0; i < NoThreads; ++i)
             {
                 contexts[i].Queue = Queue;
-                contexts[i].NoElements = N;
                 contexts[i].Elements = Elements;
+                contexts[i].NoElements = NoElements;
 
-                threads[i] = CreateThread(NULL, 0, Routine, &contexts[i], 0, NULL);
-                if (NULL == threads[i])
+                threads[i] = CreateThread(nullptr, 0, Routine, &contexts[i], 0, nullptr);
+                if (!threads[i])
                 {
-                    WaitForMultipleObjects(i, threads, TRUE, INFINITE);
-                    goto CleanUp;
+                    WaitForMultipleObjects(i, &threads[0], TRUE, INFINITE);
+                    Assert::Fail();
                 }
             }
 
-            WaitForMultipleObjects(X, threads, TRUE, INFINITE);
-            shouldFail = false;
-
-        CleanUp:
-            if (threads != NULL)
-            {
-                DS_Free(threads, QUEUE_ELMENT_TEST_TAG);
-            }
-
-            if (contexts != NULL)
-            {
-                DS_Free(contexts, QUEUE_ELMENT_TEST_TAG);
-            }
-
-            if (shouldFail)
-            {
-                Assert::Fail();
-            }
+            WaitForMultipleObjects(NoThreads, &threads[0], TRUE, INFINITE);
         }
     };
 }
